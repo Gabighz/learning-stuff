42e01f6fbcbf64f4a8e85a263bff27077bc9ef6b
Gabighz <ghiuzangabriel2@gmail.com>
2022-06-16 23:39:21 +0300

    fixed bugs in the dapp project

--- Ethereum/README.md
+++ Ethereum/README.md
@@ -2,12 +2,8 @@ This has been done as part of the [Ethereum and Solidity: The Complete Developer
 
 Inbox, lottery, and lottery-react are simple, toy projects (still found them super cool and fun).
 
-**Kickstart** is an (almost*) production-ready dApp and could be deployed in the wild. It's a demonstration of a possibly more elegant solution than a centralized app for Kickstarter's issues with scammers and failed projects. 
+**Kickstart** is a production-ready dApp and could be deployed in the wild. It's a demonstration of a possibly more elegant solution than a centralized app for Kickstarter's issues with scammers and failed projects. 
 
 We leverage the nature of smart contracts to control how the 'idea person'/manager can use the raised funds. The funds are sent to an Ethereum contract. This manager can create spending requests that attempt to send money to some external addresses (ideally vendors) and the contributors vote on whether to approve it.
 
 Of course, it's not a perfect solution, as it assumes that the contributors are going to vote, that the vendor is not malicious, and that the address is legitimate - possibly other drawbacks too.
-
-*:
-- There are some bugs to fix regarding the front-end of the approval mechanism for spending requests.
-- The visual design is pretty amateurish
--- Ethereum/kickstart/components/RequestRow.js
+++ Ethereum/kickstart/components/RequestRow.js
@@ -29,19 +29,25 @@ class RequestRow extends Component {
         const readyToFinalize = request.approvalCount > contributorsCount / 2
 
         return (
-            <Row disabled={request.complete} positive={readyToFinalize && !request.complete}>
+            <Row 
+                disabled={request.complete}
+                positive={readyToFinalize && !request.complete}
+            >
                 <Cell>{id}</Cell>
                 <Cell>{request.description}</Cell>
                 <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
                 <Cell>{request.recipient}</Cell>
                 <Cell>{request.approvalCount}/{contributorsCount}</Cell>
-                {request.complete ? null : (
-                    <Cell><Button color="green" basic onClick={this.onApprove}>Approve</Button></Cell>
-                )}
-                {request.complete ? null : (
-                    <Cell><Button color="teal" basic onClick={this.onFinalize}>Finalize</Button></Cell>
-                )}
-                
+                <Cell>
+                    {request.complete ? null : (
+                        <Button color="green" basic onClick={this.onApprove}>Approve</Button>
+                    )}
+                </Cell>
+                <Cell>
+                    {request.complete ? null : (
+                        <Button color="teal" basic onClick={this.onFinalize}>Finalize</Button>
+                    )}
+                </Cell>
             </Row>
         )
     }
