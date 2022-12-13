ee3bdbaa1a8b8b45aa5dfeda207aa6f95688422f
Gabighz <ghiuzangabriel2@gmail.com>
2022-12-13 03:22:55 +0200

    decimals instead of floats

--- quantitative_finance/investment_return.py
+++ quantitative_finance/investment_return.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+from decimal import Decimal
 
 def get_data_from_broker():
     NotImplemented
 
-def net_return(initial_investment: float, current_value: float):
+def net_return(initial_investment, current_value):
     return (current_value - initial_investment) / initial_investment
 
 if __name__ == '__main__':
 
-    initial_investment = float(input("Initial investment: "))
-    current_value = float(input("Current value of investment: "))
+    initial_investment = Decimal(input("Initial investment: "))
+    current_value = Decimal(input("Current value of investment: "))
     as_percentage = 100 * net_return(initial_investment, current_value)
 
     print(f'Net return: {as_percentage:.2f}%')
--- quantitative_finance/present_future_values.py
+++ quantitative_finance/present_future_values.py
@@ -1,4 +1,9 @@
-from math import exp
+#!/usr/bin/python3
+import decimal
+from decimal import Decimal
+
+decimal.getcontext().rounding = decimal.ROUND_HALF_EVEN # Banker's rounding
+decimal.getcontext().prec = 28 # Precision to 28 decimal places
 
 '''
 Keep in mind that the gross return of an investment at a periodically compounded interest rate equals the
@@ -15,24 +20,25 @@ def present_discrete_value(x, r, n):
     return x * (1 + r) ** -n
 
 def future_continuous_value(x, r, t):
-    return x * exp(r * t)
+    return x * Decimal.exp(r * t)
 
 def present_continuous_value(x, r, t):
-    return x * exp(-r * t)
+    return x * Decimal.exp(-r * t)
 
 def periodic_compounding(x, r, t, m):
     return (1 + r / m) ** (m * t) * x
 
 if __name__ == '__main__':
 
-    x = float(input("Principal: "))
-    r = float(input("Annual interest rate expressed as a floating-point number: "))
-    t = float(input("Period in years: "))
+    # NOTE: Decimals instead of floats here are highly desirable for their exact finite representations
+    x = Decimal(input("Principal: "))
+    r = Decimal(input("Annual interest rate expressed as a fixed-point number: "))
+    t = Decimal(input("Period in years: "))
     m = input("Compouding period (optional): ")
 
-    print("Future value (discrete model) of x: %.2f" % future_discrete_value(x, r, t))
-    print("Present value (discrete model) of x: %.2f" % present_discrete_value(x, r, t))
-    print("Future value (continuous model) of x: %.2f" % future_continuous_value(x, r, t))
-    print("Present values (continuous model) of x: %.2f" % present_continuous_value(x, r, t))
+    print(f"Future value (discrete model) of x: {future_discrete_value(x, r, t):.2f}")
+    print(f"Present value (discrete model) of x: {present_discrete_value(x, r, t):.2f}")
+    print(f"Future value (continuous model) of x: {future_continuous_value(x, r, t):.2f}")
+    print(f"Present values (continuous model) of x: {present_continuous_value(x, r, t):.2f}")
     if m:
-        print("Future value (periodic compounding) of x: %.2f" % periodic_compounding(x, r, t, float(m)))
+        print(f"Future value (periodic compounding) of x: {periodic_compounding(x, r, t, Decimal(m)):.2f}")
